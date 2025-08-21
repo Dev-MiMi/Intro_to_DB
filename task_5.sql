8b909bdfe5681d81af5d4bd6aec659225ed3f98d
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-08-21 21:47:05 +0100

    _:*€

--- task_5.sql
+++ task_5.sql
@@ -41,5 +41,5 @@ CREATE TABLE Order_Details(
 
 INSERT INTO customer (customer_id, customer_name, email, address);
 VALUES(
-	1, 'Cole Baidoo', 'cbaidoo@sandtech.com', 123 Happiness Ave)
+	1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
 
