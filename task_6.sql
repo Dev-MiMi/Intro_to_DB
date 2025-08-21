c6cf37a694f4d5487d2dedc42092edf249ecb5c5
Dev-MiMi <miracleolagundoye1@gmail.com>
2025-08-21 22:09:54 +0100

    _:*€

--- task_6.sql
+++ task_6.sql
@@ -40,9 +40,9 @@ CREATE TABLE Order_Details(
 
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
 
 
